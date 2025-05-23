7ae9334d6123f400f4afe8ca5a93a9ef437cdf87
StormVansh <130889019+StormVansh@users.noreply.github.com>
2025-05-23 00:31:40 -0600

    Add files via upload

---
+++ Untitled-8.cs
@@ -0,0 +1,9 @@
+// Model/Document.cs (in MAUI project)
+public class Document
+{
+    public int Id { get; set; }
+    public string FileName { get; set; }
+    public string FilePath { get; set; }
+    public DateTime UploadDate { get; set; }
+    public string UploadedBy { get; set; }
+}
