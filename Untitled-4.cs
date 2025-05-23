f4662a759856166237ca4337c0f2436952615f8b
StormVansh <130889019+StormVansh@users.noreply.github.com>
2025-05-23 00:29:12 -0600

    Add files via upload

---
+++ Untitled-4.cs
@@ -0,0 +1,7 @@
+// Data/AppDbContext.cs
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+    public DbSet<Document> Documents { get; set; }
+}
---
+++ program.cs
@@ -0,0 +1,26 @@
+// Program.cs
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors();
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
